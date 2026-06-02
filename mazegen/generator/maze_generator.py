1dd7af108c4d026faacacb495d20866dcd4fb932
ThomasMattelaer <mattelaer.thomas@hotmail.com>
2026-06-02 20:04:09 +0200

    last psu from 2/06 at 20:00

--- mazegen/generator/maze_generator.py
+++ mazegen/generator/maze_generator.py
@@ -41,40 +41,48 @@ class MazeGenerator():
                                       ]
         return tab
 
-    def render_tab(self, tab: list[list[int | str]]) -> None:
+    def render_tab(self, tab):
+        TOP = 1
+        RIGHT = 2
+        BOTTOM = 4
+        LEFT = 8
         color = random.randint(0, 256)
-
         for row in range(len(tab)):
-            len_row = len(tab[row])
-            #  high
-            for cell in range(len_row):
-                value = tab[row][cell]
-                if isinstance(value, str):
-                    value = int(value, 16)
+            # HIGH
+            for cell, value in enumerate(tab[row]):
+                if isinstance(tab[row][cell], str):
+                    value = int(tab[row][cell], 16)
                 if (row == 0 and cell == 0):
-                    print(f"\x1b[38;5;{color}m{chr(9581) }\x1b[0m", end="")
-                if (value & 1):
-                    print(f"\x1b[38;5;{color}m{chr(9472) * 2}\x1b[0m", end="")
+                    print(f"\x1b[38;5;{color}m╭\x1b[0m", end="")
+                if (value & TOP):
+                    print(f"\x1b[38;5;{color}m──\x1b[0m", end="")
                 else:
                     print("  ", end="")
+
+            # RIGHT BORDER
             if row == 0:
-                print(f"\x1b[38;5;{color}m{chr(9582)}\x1b[0m", end="")
+                print(f"\x1b[38;5;{color}m╮\x1b[0m", end="")
             elif row == self._height - 1:
-                print(f"\x1b[38;5;{color}m{chr(9584)}\x1b[0m", end="")
+                print(f"\x1b[38;5;{color}m╯\x1b[0m", end="")
             else:
-                print(f"\x1b[38;5;{color}m{chr(9474)}\x1b[0m", end="")
+                print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
             print()
-            #  mid
+
+            # MID
             for cell, value in enumerate(tab[row]):
                 if isinstance(value, str):
                     value = int(value, 16)
-            if (value & 8):
-                print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
-            elif (value & 4):
-                print(f"\x1b[38;5;{color}m──\x1b[0m", end="")
+                if (value & LEFT):
+                    print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
+                if value & BOTTOM:
+                    print(f"\x1b[38;5;{color}m──\x1b[0m", end="")
+                else:
+                    print("  ", end="")
+            if row == self._height - 1:
+                print(f"\x1b[38;5;{color}m╯\x1b[0m", end="")
             else:
-                print("  ", end="")
-
+                print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
+            print()
 
 if __name__ == "__main__":
 
