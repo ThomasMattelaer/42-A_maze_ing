142b06bf5e84b1b9c2aff4014a2fef438dd2dfb2
ThomasMattelaer <mattelaer.thomas@hotmail.com>
2026-06-03 18:40:23 +0200

    generation of the matrix + 42 Pattern

--- mazegen/generator/maze_generator.py
+++ mazegen/generator/maze_generator.py
@@ -20,72 +20,125 @@ class MazeGenerator():
         self._output_file = output_file
         self._perfect = perfect
 
-    def draw_rectangle(self) -> None:
-        color = random.randint(0, 256)
-        print(f"\x1b[38;5;{color}m{chr(9581) + chr(9472) * (self._width * 2)}"
-              + f"{chr(9582)}\x1b[0m")
+    # def draw_rectangle(self) -> None:
+    #     color = random.randint(0, 256)
+    #     print(f"\x1b[38;5;{color}m{chr(9581) + chr(9472) * (self._width * 2)}"
+    #           + f"{chr(9582)}\x1b[0m")
 
-        for i in range(self._height):
-            print(f"\x1b[38;5;{color}m{chr(9474)}{' ' * (self._width * 2)}" +
-                  f"{chr(9474)}\x1b[0m")
+    #     for i in range(self._height):
+    #         print(f"\x1b[38;5;{color}m{chr(9474)}{' ' * (self._width * 2)}" +
+    #               f"{chr(9474)}\x1b[0m")
 
-        print(f"\x1b[38;5;{color}m{chr(9584) + chr(9472) * (self._width * 2)}"
-              + f"{chr(9583)}\x1b[0m")
-
-    def tab(self) -> list[list[int | str]]:
-        tab: list[list[int | str]] = [[9, 5, 5, 5, 3],
-                                      [8, 0, 5, 0, 2],
-                                      [8, 6, 5, "C", 2],
-                                      [8, 0, 5, 0, 2],
-                                      ["C", 5, 5, 5, 6],
-                                      ]
-        return tab
-
-    def render_tab(self, tab):
-        TOP = 1
-        RIGHT = 2
-        BOTTOM = 4
-        LEFT = 8
-        color = random.randint(0, 256)
-        for row in range(len(tab)):
-            # HIGH
-            for cell, value in enumerate(tab[row]):
-                if isinstance(tab[row][cell], str):
-                    value = int(tab[row][cell], 16)
-                if (row == 0 and cell == 0):
-                    print(f"\x1b[38;5;{color}m╭\x1b[0m", end="")
-                if (value & TOP):
-                    print(f"\x1b[38;5;{color}m──\x1b[0m", end="")
+    #     print(f"\x1b[38;5;{color}m{chr(9584) + chr(9472) * (self._width * 2)}"
+    #           + f"{chr(9583)}\x1b[0m")
+    def init_maze(self) -> list[list[int]]:
+        matrix: list[list[int]] = []
+        for row in range(self._height * 2 + 1):
+            columns: list[int] = []
+            for col in range(self._height * 2 + 1):
+                if (col % 2 == 0 or row % 2 == 0):
+                    columns.append(1)
                 else:
-                    print("  ", end="")
+                    columns.append(2)
+            matrix.append(columns)
+        return matrix
 
-            # RIGHT BORDER
-            if row == 0:
-                print(f"\x1b[38;5;{color}m╮\x1b[0m", end="")
-            elif row == self._height - 1:
-                print(f"\x1b[38;5;{color}m╯\x1b[0m", end="")
-            else:
-                print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
-            print()
+    def setup42(self, maze: list[list[int]]) -> None:
+        height_42 = 6
+        width_42 = 7
 
-            # MID
-            for cell, value in enumerate(tab[row]):
-                if isinstance(value, str):
-                    value = int(value, 16)
-                if (value & LEFT):
-                    print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
-                if value & BOTTOM:
-                    print(f"\x1b[38;5;{color}m──\x1b[0m", end="")
+        PATTERN_42 = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (1, 2), (0, 2),
+                      (3, 2), (4, 2), (5, 2),
+                      (0, 4), (0, 5), (0, 6), (1, 6), (2, 6), (2, 5), (2, 4),
+                      (3, 4), (4, 4), (5, 4), (5, 5), (5, 6)
+                      ]
+        start_row = (self._height // 2) - (height_42 // 2)
+        start_col = (self._width // 2) - (width_42 // 2)
+        
+        for row, col in PATTERN_42:
+            r = (start_row + row) * 2 + 1
+            c = (start_col + col) * 2 + 1
+            maze[r][c] = 5
+        print(maze)
+
+
+def get_corner(maze: list[list[int]], row: int, col: int) -> str:
+    rows, cols = len(maze), len(maze[0])
+    N, E, S, W = 1, 2, 4, 8
+
+    on_top = row == 0
+    on_bot = row == rows
+    on_left = col == 0
+    on_right = col == cols
+
+    topl = maze[row-1][col-1] if row > 0 and col > 0 else None
+    topr = maze[row-1][col] if row > 0 and col < cols else None
+    bottoml = maze[row][col-1] if row < rows and col > 0 else None
+    bottomr = maze[row][col] if row < rows and col < cols else None
+
+    vert = (topl and topl & E) or (topr and topr & W) or \
+           (bottoml and bottoml & E) or (bottomr and bottomr & W)
+
+    horiz = (topl and topl & S) or (bottoml and bottoml & N) or \
+            (topr and topr & S) or (bottomr and bottomr & N)
+
+    if on_top and on_left:
+        return '┌'
+    if on_top and on_right:
+        return '┐'
+    if on_bot and on_left:
+        return '└'
+    if on_bot and on_right:
+        return '┘'
+    if on_top:
+        if (vert):
+            return '┬'
+        return '───'
+    if on_bot:
+        if (vert):
+            return '┴'
+        return '───'
+    if on_left:
+        if (horiz):
+            return '├'
+        return '│'
+    if on_right:
+        if (horiz):
+            return '┤'
+        return '│'
+    if (vert and horiz):
+        return '┼'
+    return "   "
+
+
+def render_tab(maze: list[list[int]]) -> None:
+    rows = len(maze)
+    cols = len(maze[0])
+    color = random.randint(0, 256)
+
+    for row in range(rows + 1):
+        line = ""
+        for col in range(cols + 1):
+            line += get_corner(maze, row, col)
+        print(f"\x1b[38;5;{color}m{line}\x1b[0m")
+
+        if row < rows:
+            line = ""
+            for col in range(cols):
+                if maze[row][col] & 8:
+                    line += "│"
                 else:
-                    print("  ", end="")
-            if row == self._height - 1:
-                print(f"\x1b[38;5;{color}m╯\x1b[0m", end="")
+                    line += "   "
+
+            if maze[row][cols - 1] & 2:
+                line += "│"
             else:
-                print(f"\x1b[38;5;{color}m│\x1b[0m", end="")
-            print()
+                line += "   "
+            print(line)
+
 
 if __name__ == "__main__":
 
-    maze = MazeGenerator(5, 5, (10, 2), (10, 2), "test", True)
-    tab = maze.tab()
-    maze.render_tab(tab)
+    maze = MazeGenerator(10, 10, (10, 2), (10, 2), "test", True)
+    init = maze.init_maze()
+    maze.setup42(init)
