4b919b6414784a19c30ec12ac7db30962376fb1d
ThomasMattelaer <mattelaer.thomas@hotmail.com>
2026-06-09 17:05:51 +0200

    end of the version on the 09/06

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,15 @@
 from mazegen import MazeGenerator, solve, render_maze, generate_maze
 
 
+def ask_user_action() -> None:
+    print("""1. Change the color of the Maze
+          2. Hide/Show the ideal path
+          3. Re-generate the maze""")
+
+
 if __name__ == "__main__":
     print("A-MAZE-ING !")
     maze_class = MazeGenerator(15, 15, (1, 3), (3, 5), "test", True, seed=81)
     maze = generate_maze(maze_class)
     solve(maze_class._entry, maze_class._exit, maze)
     render_maze(maze)
-    
